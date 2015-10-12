a2f742f0e50b16d5fecee01d6eb79cd23d646e76
Ian Lai <MrSaints@users.noreply.github.com>
2015-10-12 20:49:21 +0800

    Add gulp tasks to build docs assets

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ gulp.task("sass", function () {
     gulp.src("./sass/**/*.scss")
         .pipe(sass({
             //outputStyle: "compressed",
-            includePaths: ["bower_components/bourbon/app/assets/stylesheets/"]
+            includePaths: ["./bower_components/bourbon/app/assets/stylesheets/"]
         }))
         .pipe(nano())
         .pipe(gulp.dest("./dist"));
@@ -16,4 +16,30 @@ gulp.task("sass:watch", function () {
     gulp.watch("./sass/**/*.scss", ["sass"]);
 });
 
-gulp.task("default", ["sass"]);
+gulp.task("sass:docs", function () {
+    gulp.src("./docs/sass/**/*.scss")
+        .pipe(sass({
+            //outputStyle: "compressed",
+            includePaths: [
+                "./bower_components/bourbon/app/assets/stylesheets",
+                "./sass"]
+        }))
+        .pipe(nano())
+        .pipe(gulp.dest("./docs/css"));
+});
+
+gulp.task("sass:docs:watch", function () {
+    gulp.watch("./docs/sass/**/*.scss", ["sass:docs"]);
+});
+
+gulp.task("copy:docs", function () {
+    gulp.src(["./dist/civil.css", "./dist/syntax.css"])
+        .pipe(gulp.dest("./docs/css"));
+
+    gulp.src("dist/civil.js")
+        .pipe(gulp.dest("./docs/js"));
+});
+
+gulp.task("default", ["build", "docs"]);
+gulp.task("build", ["sass"]);
+gulp.task("docs", ["copy:docs", "sass:docs"]);
