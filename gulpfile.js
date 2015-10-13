155d3969b1af58b04a86955263108ecfa0d9cdad
Ian Lai <MrSaints@users.noreply.github.com>
2015-10-14 03:04:04 +0800

    Add comments; and run task before watching

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var nano = require('gulp-cssnano');
 
+/*
+ Build civil.css
+ */
 gulp.task("sass", function () {
     gulp.src("./sass/**/*.scss")
         .pipe(sass({
@@ -12,10 +15,13 @@ gulp.task("sass", function () {
         .pipe(gulp.dest("./dist"));
 });
 
-gulp.task("sass:watch", function () {
+gulp.task("sass:watch", ["sass"], function () {
     gulp.watch("./sass/**/*.scss", ["sass"]);
 });
 
+/*
+ Build docs
+ */
 gulp.task("sass:docs", function () {
     gulp.src("./docs/sass/**/*.scss")
         .pipe(sass({
@@ -28,10 +34,13 @@ gulp.task("sass:docs", function () {
         .pipe(gulp.dest("./docs/css"));
 });
 
-gulp.task("sass:docs:watch", function () {
+gulp.task("sass:docs:watch", ["sass:docs"], function () {
     gulp.watch("./docs/sass/**/*.scss", ["sass:docs"]);
 });
 
+/*
+ Copy civil.css latest build for docs
+ */
 gulp.task("copy:docs", function () {
     gulp.src(["./dist/civil.css", "./dist/syntax.css"])
         .pipe(gulp.dest("./docs/css"));
